37f7b3c54e3e3ced4d78ab9e9a5bedc9d0c0623b
Vitaliy <prokopchuk.vitaliy@gmail.com>
2014-07-07 15:11:36 +0300

    nodemailer added to project

---
+++ public/js/sendmail.js
@@ -0,0 +1,22 @@
+﻿var nodemailer = require("nodemailer");
+
+var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Gmail",
+    auth: {
+        user: "prokopchuk.vitaliy@gmail.com",
+        pass: "FthuXx356789"
+    }
+});
+
+smtpTransport.sendMail({
+    from: document.getElementById('email_name').value + " " + document.getElementById('email_address_from').value, // sender address
+    to: "prokopchuk.vitaliy@gmail.com", // comma separated list of receivers
+    subject: document.getElementById('email_subject').value, // Subject line
+    text: document.getElementById('email_text').value // plaintext body
+}, function (error, response) {
+    if (error) {
+        console.log(error);
+    }else {
+        console.log("Message sent: " + response.message);
+    }
+});
